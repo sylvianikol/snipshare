78dd50f7b9bae2dae027c2a98a4261510f8983c2
sylvianikol <silviaadvert@gmail.com>
2023-11-06 16:21:55 +0200

    Created users model

---
+++ internal/models/users.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type User struct {
+	ID             int
+	Name           string
+	Email          string
+	HashedPassword []byte
+	Created        time.Time
+}
+
+type UserModel struct {
+	DB *sql.DB
+}
+
+func (m *UserModel) Insert(name, email, password string) error {
+	return nil
+}
+
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	return 0, nil
+}
